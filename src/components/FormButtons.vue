41ea9486a9f1521b2aff772bd66948130e319327
Parsa Farahani <parsafarahany821@gmail.com>
2023-07-05 23:54:05 +0330

    The form buttons which emit up on click

---
+++ src/components/FormButtons.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-if="!isEditingForm" class="flex justify-between">
+            <BaseButton class="bg-emerald-500">افزودن</BaseButton>
+            <BaseButton class="bg-gray-500" type="button" @click="$emit('cancel-add-employee')">انصراف</BaseButton>
+        </div>
+        <div v-if="isEditingForm" class="flex justify-center">
+            <BaseButton type="button" class="bg-emerald-500" @click="$emit('employee-updated')">بروزرسانی</BaseButton>
+        </div>
+    </div>
+</template>
+
+<script>
+import BaseButton from './BaseButton.vue';
+export default {
+    props: ['isEditingForm'],
+    components: { BaseButton }
+};
+</script>
+
+<style scoped></style>
